36f721d518d218548737e7dbabb6b43d7715bda3
Jyotirmaya Mahanta <jyotirmaya.mahanta@gmail.com>
2024-12-06 19:12:27 +0530

    _torch_load

--- models/mistral_nonrolling/load.py
+++ models/mistral_nonrolling/load.py
@@ -4,21 +4,14 @@ import safetensors.torch
 from models.mistral_nonrolling.transformer import Transformer, MistralConfig
 from models.mistral_nonrolling.tokenizer import Tokenizer
 
-def _load_state_dict(folder: str):
+def _torch_load(folder: str):
   pt_model_file = pathlib.Path(folder) / "consolidated.00.pth"
-  safetensors_model_file = pathlib.Path(folder) / "consolidated.safetensors"
-  assert (
-    pt_model_file.exists() or safetensors_model_file.exists()
-  ), f"Make sure either {pt_model_file} or {safetensors_model_file} exists"
-  assert not (
-    pt_model_file.exists() and safetensors_model_file.exists()
-  ), f"Both {pt_model_file} and {safetensors_model_file} cannot exist"
-  if pt_model_file.exists(): state_dict = torch.load(str(pt_model_file), mmap=True, weights_only=True)
-  else: state_dict = safetensors.torch.load_file(str(safetensors_model_file))
+  assert pt_model_file.exists(), f"Make sure either {pt_model_file} exists"
+  state_dict = torch.load(str(pt_model_file), mmap=True, weights_only=True)
   return state_dict
 
 def build(folder: str, max_seq_len: int, max_batch_size: int):
-  state_dict = _load_state_dict(folder)
+  state_dict = _torch_load(folder)
   with open(pathlib.Path(folder) / "params.json", "r") as f:
     config = MistralConfig.build(max_seq_len, max_batch_size, **json.load(f))
   torch.set_default_dtype(config.torch_dtype)
--- models/mistral_rolling/load.py
+++ models/mistral_rolling/load.py
@@ -1,12 +1,12 @@
 import pathlib, json, torch
 
-from models.mistral_nonrolling.load import _load_state_dict
+from models.mistral_nonrolling.load import _torch_load
 from models.mistral_nonrolling.config import MistralConfig
 from models.mistral_nonrolling.tokenizer import Tokenizer
 from models.mistral_rolling.transformer import Transformer
 
 def build(folder: str, max_seq_len: int, max_batch_size: int):
-  state_dict = _load_state_dict(folder)
+  state_dict = _torch_load(folder)
   with open(pathlib.Path(folder) / "params.json", "r") as f:
     config = MistralConfig.build(max_seq_len, max_batch_size, **json.load(f))
   torch.set_default_dtype(config.torch_dtype)
