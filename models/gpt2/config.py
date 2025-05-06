d061c027553db3ba533eb8639f4487c3e90ff708
Jyotirmaya Mahanta <jyotirmaya.mahanta@gmail.com>
2025-05-06 08:47:47 +0530

    hotfix: gpt2 params

--- models/gpt2/config.py
+++ models/gpt2/config.py
@@ -10,7 +10,7 @@ class GPTConfig:
   norm_eps: float = 1e-5
 
 CONFIGS = {
-  'gpt2':         dict(n_layer=24, n_head=16, n_embd=1024),  # 124M params
+  'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
   'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024),  # 350M params
   'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
   'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
